0dc211c5c8e2f506d53d5ab06ff10bf8a672bf00
Allison Howard <alliemhow@gmail.com>
2019-01-10 13:17:18 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,7 @@ def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    movie.title
-    movie.release_date
+
   end
 end
 
